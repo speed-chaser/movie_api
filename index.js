0a15d892a98309e99aae46170d5950e74d1de734
speed-chaser <aychasepro@gmail.com>
2023-08-02 12:04:32 -0400

    changed CORS route configuration

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ let allowedOrigins = [
 ];
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:1234");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
